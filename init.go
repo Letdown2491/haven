587afe7bc7f81ac68f767461fcfc07cbab44f600
Letdown2491 <68875499+Letdown2491@users.noreply.github.com>
2025-08-28 21:09:01 -0600

    Updated dependecies.

--- init.go
+++ init.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/fiatjaf/khatru"
 	"github.com/nbd-wtf/go-nostr"
-	"github.com/letdown2491/haven/internal/whitelist"
+	"github.com/Letdown2491/haven/tree/feature/whitelist-owner/internals/whitelist"
 )
 
 // ------------------------------------------------------------------------------------
